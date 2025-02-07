6ee0829d9d479ffe71b0d2a541677e3cfb04c106
Alisher Sharipov <alishersharipov670@gmail.com>
2025-02-07 19:56:52 +0500

    db password update

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 let db = await mysql.createConnection({
    host: "localhost",
    user: "root",
-   password: "953901313",
+   password: "12345",
    database: "wallpaperstore",
    waitForConnections: true,
    connectionLimit: 10,
