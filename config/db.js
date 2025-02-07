7bf8cdcb5fd39a82336f8681f4bee4855ee3e473
Alisher Sharipov <alishersharipov670@gmail.com>
2025-02-07 20:26:10 +0500

    db updated

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise";
 
-let db = await mysql.createConnection({
-   host: "localhost",
+const db = mysql.createPool({
+   host: "51.44.160.108",
    user: "root",
    password: "12345",
    database: "wallpaperstore",
