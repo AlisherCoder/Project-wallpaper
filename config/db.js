517b6e58b5fdcacb1d19050cce8747c4ed50bd6c
Ubuntu <ubuntu@ip-172-26-3-145.eu-west-3.compute.internal>
2025-02-08 07:12:13 +0000

    aa

--- config/db.js
+++ config/db.js
@@ -1,14 +1,22 @@
 import mysql from "mysql2/promise";
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 const db = mysql.createPool({
 =======
 let db = await mysql.createConnection({
 >>>>>>> 7c35a70 (server)
    host: "51.44.160.108",
+=======
+const db = mysql.createPool({
+   host: "localhost",
+>>>>>>> a77d53f (update)
    user: "root",
    password: "12345",
    database: "wallpaperstore",
+   waitForConnections: true,
+   connectionLimit: 10,
+   queueLimit: 0,
 });
 console.log("MySQL Connection successfully!");
 export default db;
